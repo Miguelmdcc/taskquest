4cbb3b21521201d4215bde86b57b1fbe2ed08575
Miguelmdcc <108490537+Miguelmdcc@users.noreply.github.com>
2026-04-12 21:29:57 -0300

    fix: fixing .bat setup

--- setup.bat
+++ setup.bat
@@ -1,37 +1,35 @@
 @echo off
-:: Configura o terminal para aceitar acentos (UTF-8)
+:: Configura o terminal para UTF-8
 chcp 65001 >nul
 
-echo "Verificando se o Python esta instalado..."
+echo Verificando se o Python esta instalado...
 where python >nul 2>nul
 if %errorlevel% neq 0 (
     echo [ERRO] Python nao encontrado. 
-    echo Por favor, instale-o em: https://www.python.org/downloads/ e tente novamente.
+    echo Por favor, instale-o em: https://www.python.org/downloads/
     pause
     exit /b 1
 )
 
 python --version
 
-echo "------------------------------------------"
-echo "Criando ambiente virtual..."
+echo ------------------------------------------
+echo Criando ambiente virtual...
 if not exist venv (
     python -m venv venv
 )
 
-echo "Atualizando o pip e instalando dependencias..."
-:: Usamos o caminho direto do python do venv para não depender de ativação agora
+echo Atualizando o pip e instalando dependencias...
 venv\Scripts\python.exe -m pip install --upgrade pip
 if exist requirements.txt (
     venv\Scripts\pip install -r requirements.txt
 )
 
-:: DETECÇÃO DE SHELL: O venv vai ficar ativo agora
-:: Se a variável %PSModulePath% existir, você provavelmente está no PowerShell
+:: DETECCAO DE SHELL
 if not "%PSModulePath%"=="" (
     echo [PowerShell detectado] Ativando ambiente virtual...
-    powershell -NoExit -ExecutionPolicy Bypass -Command "& {.\venv\Scripts\Activate.ps1; python seed.py; Write-Host 'Bem-vindo ao TaskQuest! Sua aventura esta configurada e pronta para ser vivida! Vá e comece suas tasks no mundo de TaskQuest!' -ForegroundColor Green}"
+    powershell -NoExit -ExecutionPolicy Bypass -Command "& {.\venv\Scripts\Activate.ps1; python seed.py; Write-Host 'Bem-vindo ao TaskQuest! Pronto para a aventura!' -ForegroundColor Green}"
 ) else (
     echo [CMD detectado] Ativando ambiente virtual...
-    cmd /k "venv\Scripts\activate.bat && python seed.py && echo Bem-vindo ao TaskQuest! Sua aventura esta configurada e pronta para ser vivida! Vá e comece suas tasks no mundo de TaskQuest!"
+    cmd /k "venv\Scripts\activate.bat && python seed.py && echo Bem-vindo ao TaskQuest! Pronto para a aventura!"
 )
